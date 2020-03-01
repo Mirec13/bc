afd4e0e75d3db714a52a7b87ce4358c3aac2ec2b
Mirec13 <balgamiroslav@gmail.com>
2020-03-01 22:36:37 +0100

    we can load the file now

--- functions.py
+++ functions.py
@@ -3,13 +3,13 @@ import csv
 
 def load_file(file_name):
     # append the path to the file
-    file_name = "datasets" + "/" + file_name;
+    file_name = "datasets" + "/" + file_name
 
     # load the csv file in 2D list
     with open(file_name, newline='') as csv_file:
-        data = list(csv.reader(csv_file));
+        data = list(csv.reader(csv_file))
 
     # delete the name of the columns from list
-    del data[0];
+    del data[0]
 
-    return data;
+    return data
--- search_epistasis.py
+++ search_epistasis.py
@@ -1,14 +1,21 @@
 import functions as f
 from collections import namedtuple
 
+
 def search(file_name, prob_switch, number_of_iter, number_of_population, number_of_repetition, p_value, num_of_epi):
-    snp_data = namedtuple("snp_data", "sample_size snp_size data");
+    snp_data = namedtuple("snp_data", "sample_size snp_size data state")
 
-    snp_data.data = f.load_file("testFile.txt");
-    snp_data.snp_size = len(snp_data.data[0]);
-    snp_data.sample_size = len(snp_data.data);
+    # initialize SNP data
+    snp_data.data = f.load_file(file_name)
+    snp_data.snp_size = len(snp_data.data[0]) - 1
+    snp_data.sample_size = len(snp_data.data)
+    snp_data.state = []
+    for row in snp_data.data:
+        snp_data.state.append(row[snp_data.snp_size])
+        del row[snp_data.snp_size]
+        print(row)
 
-    print(snp_data.sample_size, snp_data.snp_size)
+    print(snp_data.sample_size, snp_data.snp_size, snp_data.state)
 
 
 def start():
