a0ae7cbfa703cb40ae172884025a7e015c628298
Mirec13 <balgamiroslav@gmail.com>
2020-03-03 00:08:04 +0100

    compute observed and expected values

--- functions.py
+++ functions.py
@@ -2,14 +2,60 @@ import csv
 
 
 def load_file(file_name):
-    # append the path to the file
-    file_name = "datasets" + "/" + file_name
+    try:
+        # append the path to the file
+        file_name = "datasets" + "/" + file_name
 
-    # load the csv file in 2D list
-    with open(file_name, newline='') as csv_file:
-        data = list(csv.reader(csv_file))
+        # load the csv file in 2D list
+        with open(file_name, newline='') as csv_file:
+            data = list(csv.reader(csv_file))
 
-    # delete the name of the columns from list
-    del data[0]
+        # delete the name of the columns from list
+        del data[0]
 
-    return data
+        # convert data's components to int
+        y = len(data)
+        x = len(data[0])
+        for i in range(y):
+            for j in range(x):
+                data[i][j] = int(data[i][j])
+
+        return data
+    except FileNotFoundError:
+        data = False
+        return data
+
+
+def init_first_population():
+    pass
+
+
+def compute_o_and_e_values(data, sample_size, loci, observed_value, expected_value, state):
+    comb_number = 0
+
+    # compute the observed value for every combination's state
+    for i in range(3):
+        for j in range(3):
+            for k in range(sample_size):
+                if (data[k][loci[0]] == i) and (data[k][loci[1]] == j):
+                    if state[k] == 0:
+                        observed_value[comb_number][0] += 1
+                    else:
+                        observed_value[comb_number][1] += 1
+            comb_number += 1
+
+    # compute the expected value
+    for i in range(comb_number):
+        expected_value[i] = ((observed_value[i][0] + observed_value[i][1]) / 2)
+
+
+def k2_score():
+    pass
+
+
+def gi_score():
+    pass
+
+
+def g_test():
+    pass
--- search_epistasis.py
+++ search_epistasis.py
@@ -3,23 +3,44 @@ from collections import namedtuple
 
 
 def search(file_name, prob_switch, number_of_iter, number_of_population, number_of_repetition, p_value, num_of_epi):
+    # define structs
     snp_data = namedtuple("snp_data", "sample_size snp_size data state")
+    flower = [namedtuple("flower", "loci observed_value expected_value objective_function")] * number_of_population
+    comb = pow(3, num_of_epi)
+    for i in flower:
+        i.observed_value = [[0 for x in range(2)] for y in range(comb)]
+        i.expected_value = [0] * comb
+        i.loci = [0] * num_of_epi
+        i.objective_function = [0.0] * 2
 
-    # initialize SNP data
-    snp_data.data = f.load_file(file_name)
-    snp_data.snp_size = len(snp_data.data[0]) - 1
-    snp_data.sample_size = len(snp_data.data)
-    snp_data.state = []
-    for row in snp_data.data:
-        snp_data.state.append(row[snp_data.snp_size])
-        del row[snp_data.snp_size]
-        print(row)
+    while True:
 
-    print(snp_data.sample_size, snp_data.snp_size, snp_data.state)
+        # initialize SNP data
+        snp_data.data = f.load_file(file_name)
+
+        # if we do not have any data end the loop
+        if not snp_data.data:
+            break
+
+        snp_data.snp_size = len(snp_data.data[0]) - 1
+        snp_data.sample_size = len(snp_data.data)
+        snp_data.state = []
+        for row in snp_data.data:
+            snp_data.state.append(row[snp_data.snp_size])
+            del row[snp_data.snp_size]
+
+        # init manually one flower for test purposes
+        flower[0].loci[0] = 0
+        flower[0].loci[1] = 1
+        f.compute_o_and_e_values(snp_data.data, snp_data.sample_size, flower[0].loci,
+                                 flower[0].observed_value, flower[0].expected_value, snp_data.state)
+        print(flower[0].observed_value, flower[0].expected_value)
+
+        break
 
 
 def start():
-    search("testFile.txt", 1, 2, 3, 4, 5, 6);
+    search("testFile.txt", 1, 2, 1, 4, 5, 2)
 
 
-start();
+start()
